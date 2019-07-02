7730bf432797b0130d52d014ee904975730edea3
gy6177 <18735106177@163.com>
2019-07-02 16:54:39 +0800

    初次提交

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by IMAC on 2019/7/2.
+//  Copyright © 2019 IMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by IMAC on 2019/7/2.
+//  Copyright © 2019 IMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
